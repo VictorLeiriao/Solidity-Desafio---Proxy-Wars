138a309f2181e90357ed18b65f6078f4055eb33d
Victor Leirião <victor.augusto.1907@icloud.com>
2026-03-18 16:51:09 -0300

    ...

---
+++ hardhat.config.ts
@@ -0,0 +1,52 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "cancun",
+          optimizer: {         
+            enabled: true,    
+            runs: 200,
+          },
+        }
+      },
+      production: {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "cancun",
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    }, 
+    polygon: {
+      type: "http",
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: ["85cd83a2a7dec0aa7d430475e76782014b78da2a4d632685015a5bca430492b0"],
+      chainType: "l1", 
+    },
+  },
+});
