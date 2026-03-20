83452172b63053312655058038da4b9050ede73e
Victor Leirião <victor.augusto.1907@icloud.com>
2026-03-20 17:54:54 -0300

    ...

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 
-// Usamos process.env no lugar do configVariable do Hardhat 3
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// 2. Resgata as variáveis. Se não encontrar, usa uma string vazia para não quebrar o código de cara.
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+const AMOY_RPC_URL = process.env.AMOY_RPC_URL || "https://rpc-amoy.polygon.technology";
+const AMOY_PRIVATE_KEY = process.env.AMOY_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -23,10 +29,10 @@ const config: HardhatUserConfig = {
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
     polygonAmoy: {
-      url: "https://rpc-amoy.polygon.technology",
-      accounts: ["85cd83a2a7dec0aa7d430475e76782014b78da2a4d632685015a5bca430492b0"],
+      url: AMOY_RPC_URL,
+      accounts: [AMOY_PRIVATE_KEY],
     },
   },
 };
 
-export default config;  
+export default config;
