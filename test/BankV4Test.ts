66785ae1a5c422d009ad8938720a669c590b1d89
Victor Leirião <victor.augusto.1907@icloud.com>
2026-03-22 18:46:46 -0300

    ...

--- test/BankV4Test.ts
+++ test/BankV4Test.ts
@@ -33,13 +33,13 @@ describe("Bank Proxy Wars - Testes V4 (KYC e Whitelist)", function () {
   it("2. Deve barrar menores de idade no momento do cadastro", async function () {
     // Pedrinho tenta se cadastrar e a ficha na memória rejeita ele
     await expect(
-      bankProxy.connect(user1).solicitarCadastro("Pedrinho", 17, "Brasil")
-    ).to.be.revertedWith("Acesso negado: Menor de idade");
+      bankProxy.connect(user1).registerRequest("Pedrinho", 17, "Brasil")
+    ).to.be.revertedWith("Voce deve ter mais de 18 anos para se registrar");
   });
 
   it("3. Deve validar a memoria temporaria, salvar na Whitelist e liberar acesso", async function () {
     // João (25) faz o cadastro e gasta Gás apenas para a transação
-    await bankProxy.connect(user1).solicitarCadastro("Joao", 25, "Brasil");
+    await bankProxy.connect(user1).registerRequest("Joao", 25, "Brasil");
 
     // Lemos a blockchain para ver se o nome dele ficou salvo? NÃO! Apenas um booleano:
     const estaAprovado = await bankProxy.isWhitelisted(user1.address);
@@ -54,7 +54,7 @@ describe("Bank Proxy Wars - Testes V4 (KYC e Whitelist)", function () {
 
   it("4. O Admin deve conseguir congelar a conta de um mau ator", async function () {
     // O Hacker engana o cadastro dizendo que tem 30 anos
-    await bankProxy.connect(hacker).solicitarCadastro("Sr. Hacker", 30, "Russia");
+    await bankProxy.connect(hacker).registerRequest("Sr. Hacker", 30, "Russia");
     
     // Ele faz um depósito
     await bankProxy.connect(hacker).deposit({ value: ethers.parseEther("5") });
